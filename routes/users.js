b3c1d3f310232fba730780c8beb3c9330c90931a
flopy02 <flo.barege@gmail.com>
2024-11-22 09:26:00 +0100

    base de données sql qui communique avec notre serveur

--- routes/users.js
+++ routes/users.js
@@ -1,16 +1,17 @@
 const express = require("express")
 const router = express.Router()
 
-const usersArray = [
-	{ id: 1, firstName: 'John', lastName: 'Doe', role: 'admin' },
-	{ id: 2, firstName: 'Jane', lastName: 'Smith', role: 'user' },
-	{ id: 3, firstName: 'Alice', lastName: 'Johnson', role: 'moderator' },
-	{ id: 4, firstName: 'Bob', lastName: 'Brown', role: 'user' },
-	{ id: 5, firstName: 'Charlie', lastName: 'Davis', role: 'admin' }
-];
+const db = require("../database")
+
 
 router.get("/users", (req, res) => {
-        res.json(usersArray)
+    db.all('SELECT * FROM users', [], (err, rows) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+        } else {
+          res.json(rows);
+        }
+      });
 })
 
 // PUT : MODIFIER un utilisateur en fonction de son ID
