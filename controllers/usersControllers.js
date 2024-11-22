c67c75e77ed49825c50a004ef4dbff16ea7dd1db
flopy02 <flo.barege@gmail.com>
2024-11-22 09:38:21 +0100

    Rendre notre API plus modulaire

---
+++ controllers/usersControllers.js
@@ -0,0 +1,19 @@
+//(req, res) => {
+	//db.all("SELECT * FROM users", [], (err, rows) => {
+		//if (err) {
+			//res.status(500).json({ error: err.message })
+		//} else {
+			//res.json(rows)
+		//}
+	//})
+//}
+const db = require("../database")
+exports.getAllUsers = function (req, res) {
+	db.all("SELECT * FROM users", [], (err, rows) => {
+		if (err) {
+			res.status(500).json({ error: err.message })
+		} else {
+			res.json(rows)
+		}
+	})
+}
